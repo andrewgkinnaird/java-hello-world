10505f816c53a641311171e37b8695b00817071a
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2021-05-22 13:13:43 +0000

    added successfully tested RandomWord.java

--- RandomWord.java
+++ RandomWord.java
@@ -1,24 +1,29 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
-// for now, until I can configure classpath for vscode/gitpod, use "java -cp als¥gs4.jar RandomWord.java"
+// for now, until I can configure classpath for vscode/gitpod, use "java -cp algs4.jar RandomWord.java"
 // to make use of the princeton library
 
-class RandomWord {
-    public static void main(String[] args){
+public class RandomWord {
+    public static void main(String[] args)
+    {
         String champion = "Champion";
-        
-        for(var i = 0; i < args.length; i++){
-            double p = 1.0/(i+1);
-            System.out.println(p);
-            boolean coinflip = StdRandom.bernoulli(p);
-            if(coinflip){
-                
-                champion = args[i];
-                System.out.println(champion);
-            }
-        }
+        int j = 1;
 
-        System.out.println(champion);
+        while (!StdIn.isEmpty())
+        {
+            String currentWord = StdIn.readString();
+            double p = 1.0/j;
+            boolean coinflip = StdRandom.bernoulli(p);
+            if (coinflip)
+            {
+                
+                champion = currentWord;
+            }
+            j++;
+        }
+            
+
+        StdOut.println(champion);
     }
 }
